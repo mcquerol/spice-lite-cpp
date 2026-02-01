11bca38dd21cb87f611c5b5572f37c13f7aa88f6
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-01 22:19:19 +0100

    implement Resistor class methods

--- myCode/Resistor.cpp
+++ myCode/Resistor.cpp
@@ -7,20 +7,34 @@
 
 #include "Resistor.h"
 
-Resistor::Resistor() {
+Resistor::Resistor(std::string& name, float resistance, float tolerance) {
+	this->name = name;
+	this->resistance = resistance;
+	this->tolerance = tolerance;
 }
 
 Resistor Resistor::operator +(Resistor &other) {
+	return this->resistance + other.getResistance();
 }
 
 Resistor Resistor::operator -(Resistor &other) {
+	float total_resistance;
+	total_resistance = this->resistance - other.getName();
+	if(total_resistance < 0)
+	{
+		total_resistance = 0;
+	}
+	return total_resistance;
 }
 
 std::string& Resistor::getName() const {
+	return this->name;
 }
 
 float& Resistor::getResistance() const {
+	return this->resistance;
 }
 
 float& Resistor::getTolerance() const {
+	return this->tolerance;
 }
