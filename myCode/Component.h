e5cd19f2198844917d8f534e151688e5b854eea5
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-02 22:08:51 +0100

    create declarations for Component class

---
+++ myCode/Component.h
@@ -0,0 +1,28 @@
+/*
+ * Component.h
+ *
+ *  Created on: 2 Feb 2026
+ *      Author: mateo
+ */
+
+#ifndef COMPONENT_H_
+#define COMPONENT_H_
+
+#include <string>
+#include <vector>
+
+class Component {
+
+private:
+	std::string name;
+	std::vector<int> ndoes;
+
+public:
+	Component();
+	std::string getName() const;
+	std::vector<int> getNodes() const;
+
+	virtual ~Component();
+};
+
+#endif /* COMPONENT_H_ */
