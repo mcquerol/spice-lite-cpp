58bc0274aa44b5dd39361e76c0576551e3bf088d
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-02 22:12:01 +0100

    implement Component class methods

---
+++ myCode/Component.cpp
@@ -0,0 +1,27 @@
+/*
+ * Component.cpp
+ *
+ *  Created on: 2 Feb 2026
+ *      Author: mateo
+ */
+
+#include "Component.h"
+
+Component::Component(std::string name) {
+	// TODO Auto-generated constructor stub
+	this->name = name;
+}
+
+Component::~Component() {
+	// TODO Auto-generated destructor stub
+}
+
+
+std::string Component::getName() const {
+	return this->name;
+}
+
+std::vector<int> Component::getNodes() const {
+	return this->ndoes;
+}
+
