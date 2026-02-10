73976e1da004325942ed655d85ed5fbd8d59b7da
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-10 23:45:25 +0100

    Implement voltage class definitions

---
+++ myCode/VoltageSource.cpp
@@ -0,0 +1,15 @@
+/*
+ * VoltageSource.cpp
+ *
+ *  Created on: 10 Feb 2026
+ *      Author: mateo
+ */
+
+#include "VoltageSource.h"
+
+VoltageSource::VoltageSource(std::string name, double voltage) : name(name), voltage(voltage) {
+}
+
+double VoltageSource::getVoltage() const {
+	return voltage;
+}
