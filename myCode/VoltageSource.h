10cecee17516555244866d8159aeba117222e986
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2026-02-10 23:44:24 +0100

    Implement class declarations

---
+++ myCode/VoltageSource.h
@@ -0,0 +1,24 @@
+/*
+ * VoltageSource.h
+ *
+ *  Created on: 10 Feb 2026
+ *      Author: mateo
+ */
+
+#ifndef VOLTAGESOURCE_H_
+#define VOLTAGESOURCE_H_
+
+#include <string>
+#include "Component.h"
+
+class VoltageSource : Component{
+
+private:
+	double voltage;
+public:
+	VoltageSource(std::string name, double voltage);
+	virtual ~VoltageSource();
+	double getVoltage() const;
+};
+
+#endif /* VOLTAGESOURCE_H_ */
